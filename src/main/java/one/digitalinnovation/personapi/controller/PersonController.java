aec827ba19089fe3c448ee0dca81393448bfff0b
LuizDeSa <luizdesa2012@gmail.com>
2021-08-16 09:41:27 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.personapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/v1/people")
+public class PersonController {
+
+    public String getBook(){
+        return "API Test!";
+    }
+
+}
