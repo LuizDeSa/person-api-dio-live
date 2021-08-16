b769668d33082aa35b63aca5a0421801f7e448a0
LuizDeSa <luizdesa2012@gmail.com>
2021-08-16 10:23:02 -0300

    add GetMapping in person controller

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.personapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/v1/people")
 public class PersonController {
 
+    @GetMapping
     public String getBook(){
-        return "API Test!";
+        return "API Test!!";
     }
 
 }
